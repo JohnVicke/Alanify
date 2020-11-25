3c03d62be008808b0ea90577bddb49ceb681c470
Viktor Malmedal <viktormalmedal@gmail.com>
2020-11-25 18:08:24 +0100

    ncie56

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -27,7 +27,7 @@ const Login = () => {
           }}
           variant="contained"
           color="#f4e4cc"
-          onClick={() => window.location.assign("https://alanify.herokuaoo.com/spotify")}
+          onClick={() => window.location.assign("https://alanify.herokuapp.com/spotify")}
         >
           Continue with Spotify
         </Button>
